4fd30bc36c8fe55739f511f60036c7384d40cc9a
Adibac243410 <c243410@ugrad.iiuc.ac.bd>
2026-05-01 19:08:40 +0600

    Add files via upload

---
+++ 01knapsack.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n, W;
+    cin >> n >> W;
+
+    vector<int> wt(n), val(n);
+
+    for (int i = 0; i < n; i++) cin >> wt[i];
+    for (int i = 0; i < n; i++) cin >> val[i];
+
+    vector<int> dp(W+1, 0);
+
+    for (int i = 0; i < n; i++) {
+        for (int w = W; w >= wt[i]; w--) {
+            dp[w] = max(dp[w], val[i] + dp[w - wt[i]]);
+        }
+    }
+
+    cout << dp[W] << endl;
+
+    return 0;
+}
